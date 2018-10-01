7c7e4e8d155f2f53615bd87bd466a5bfcf779fcb
davidoso <jdavidosori96@gmail.com>
2018-09-30 21:38:34 -0500

    Giving Mapa_model an alias

--- application/controllers/mapa_controller.php
+++ application/controllers/mapa_controller.php
@@ -6,11 +6,11 @@ class Mapa_controller extends CI_Controller {
 	// Mostrar página inicial/home de los filtros y el mapa
 	public function index()
 	{
-		$this->load->model('mapa_model');
+		$this->load->model('mapa_model', 'm');
 
 		// Llenar select cbCapas
 		// https://stackoverflow.com/questions/23691396/building-a-select-with-optgroup-from-a-sql-query
-		$db_capas = $this->mapa_model->getCapas();
+		$db_capas = $this->m->getCapas();
 		$cbCapas = array();
 		foreach($db_capas as $key => $value) {
     		$cbCapas[$value['carpeta']][] = array(
@@ -20,7 +20,7 @@ class Mapa_controller extends CI_Controller {
 		$data['cbCapas'] = $cbCapas;
 
 		// Llenar select cbCampos (campos a filtrar según capa)
-		$db_campos = $this->mapa_model->getCampos();
+		$db_campos = $this->m->getCampos();
 		$cbCampos = array();
 		foreach($db_campos as $key => $value) {
     		$cbCampos[$value['capa']][] = array(
@@ -30,31 +30,31 @@ class Mapa_controller extends CI_Controller {
 		$data['cbCampos'] = $cbCampos;
 
 		// Llenar selects de la capa Bancos
-		$data['bancos_servicio'] = $this->mapa_model->getBancoServicio();
+		$data['bancos_servicio'] = $this->m->getBancoServicio();
 
 		// Llenar selects de la capa Postes
-		$data['postes_empresa'] = $this->mapa_model->getEmpresa("generales_tbl_postes");
-		$data['postes_material'] = $this->mapa_model->getMaterial("generales_tbl_postes");
-		$data['postes_cond_fisica'] = $this->mapa_model->getCondFisica("generales_tbl_postes");
+		$data['postes_empresa'] = $this->m->getEmpresa("generales_tbl_postes");
+		$data['postes_material'] = $this->m->getMaterial("generales_tbl_postes");
+		$data['postes_cond_fisica'] = $this->m->getCondFisica("generales_tbl_postes");
 
 		// Llenar selects de la capa Teléfonos
-		$data['telefonos_tipo'] = $this->mapa_model->getTelefonoCampo("tipo");
-		$data['telefonos_funciona'] = $this->mapa_model->getTelefonoCampo("funciona");
-		$data['telefonos_empresa'] = $this->mapa_model->getEmpresa("generales_tbl_telefonos_publicos");
-		$data['telefonos_cond_fisica'] = $this->mapa_model->getCondFisica("generales_tbl_telefonos_publicos");
+		$data['telefonos_tipo'] = $this->m->getTelefonoCampo("tipo");
+		$data['telefonos_funciona'] = $this->m->getTelefonoCampo("funciona");
+		$data['telefonos_empresa'] = $this->m->getEmpresa("generales_tbl_telefonos_publicos");
+		$data['telefonos_cond_fisica'] = $this->m->getCondFisica("generales_tbl_telefonos_publicos");
 
 		// Llenar selects de la capa Luminarias
-		$data['luminarias_fuente'] = $this->mapa_model->getLuminariaFuente("generales_tbl_luminarias");
-		$data['luminarias_material'] = $this->mapa_model->getMaterial("generales_tbl_luminarias");
-		$data['luminarias_cond_fisica'] = $this->mapa_model->getCondFisica("generales_tbl_luminarias");
+		$data['luminarias_fuente'] = $this->m->getLuminariaFuente("generales_tbl_luminarias");
+		$data['luminarias_material'] = $this->m->getMaterial("generales_tbl_luminarias");
+		$data['luminarias_cond_fisica'] = $this->m->getCondFisica("generales_tbl_luminarias");
 
 		// Llenar selects de la capa Monumentos
-		$data['monumentos_epoca'] = $this->mapa_model->getMonumentoCampo("epoca");
-		$data['monumentos_genero_arquitectonico'] = $this->mapa_model->getMonumentoCampo("genero_arquitectonico");
+		$data['monumentos_epoca'] = $this->m->getMonumentoCampo("epoca");
+		$data['monumentos_genero_arquitectonico'] = $this->m->getMonumentoCampo("genero_arquitectonico");
 
 		// Llenar selects de la capa Panteón
-		$data['panteon_material'] = $this->mapa_model->getMaterial("registro_civil_tbl_panteon_municipal");
-		$data['panteon_cond_fisica'] = $this->mapa_model->getCondFisica("registro_civil_tbl_panteon_municipal");
+		$data['panteon_material'] = $this->m->getMaterial("registro_civil_tbl_panteon_municipal");
+		$data['panteon_cond_fisica'] = $this->m->getCondFisica("registro_civil_tbl_panteon_municipal");
 
 
 		// Cargar página inicial/home de los filtros y el mapa
